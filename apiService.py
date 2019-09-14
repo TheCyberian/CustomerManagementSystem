b54a03acb403f6ad665980ff10b36ab8245377bb
utkarsh <utkarshraghav831@gmail.com>
2019-09-14 14:14:18 +0530

    Created base apiService

---
+++ apiService.py
@@ -0,0 +1,87 @@
+from flask import Flask, jsonify, request
+import sqlite3
+
+app = Flask(__name__)
+
+db = sqlite3.connect("Test.db", check_same_thread=False)
+c = db.cursor()
+
+
+@app.route("/addCustomer", methods=['POST'])
+def addCustomer():
+    customer = {
+        'name': request.json['name'],
+        'initial': request.json['initial'],
+        'address1': request.json['address1'],
+        'address2': request.json['address2'],
+        'address3': request.json['address3'],
+        'pincode': request.json['pincode'],
+        'phone1': request.json['phone1'],
+        'phone2': request.json['phone2'],
+        'email': request.json['email']
+    }
+
+    insert_query = "INSERT INTO customer(name, initial, address1, address2, address3, pincode, phone1, phone2, " \
+                   "email) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}' )". \
+        format(customer['name'], customer['initial'], customer['address1']
+               , customer['address2'], customer['address3'], customer['pincode']
+               , customer['phone1'], customer['phone2'], customer['email'])
+    c.execute(insert_query)
+    db.commit()
+    return jsonify(customer)
+
+
+@app.route("/addItem", methods=["POST"])
+def addItem():
+    item = {'itemName': request.json['itemName']}
+    insert_query = "INSERT INTO itemDetails(itemName) VALUES ('{}')".format(item['itemName'])
+    c.execute(insert_query)
+    db.commit()
+    return jsonify(item)
+
+
+@app.route("/addOrder", methods=["POST"])
+def addOrder():
+    order = {
+        "custId": request.json['custId'],
+        "itemNumber": request.json['itemNumber'],
+        "amountPaid": request.json['amountPaid'],
+        "amountDue": request.json['amountDue'],
+        "totalAmount": request.json['totalAmount']
+    }
+
+    insert_query = "INSERT INTO orderDetails(custId, itemNumber, amountPaid, amountDue, totalAmount)" \
+                   " VALUES ('{}', '{}', '{}', '{}', '{}' )". \
+        format(order['custId'], order['itemNumber'], order['amountPaid']
+               , order['amountDue'], order['totalAmount'])
+    c.execute(insert_query)
+    db.commit()
+    return jsonify(order)
+
+
+def getCustomers():
+    pass
+
+
+def getCustomer(custId):
+    pass
+
+
+def getItems():
+    pass
+
+
+def getItem(itemId):
+    pass
+
+
+def getOrders(custId):
+    pass
+
+
+def getItemOrders(itemId):
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- createDb.py
+++ createDb.py
@@ -7,36 +7,36 @@ db = sqlite3.connect("Test.db")
 c = db.cursor()
 
 # #creating customer table
-# c.execute("CREATE TABLE IF NOT EXISTS customer(custId INTEGER PRIMARY KEY, name TEXT, initial TEXT, address1 TEXT,"
-#           "address2 TEXT, address3 TEXT, pincode TEXT, phone1 TEXT, phone2 TEXT, email TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS customer(custId INTEGER PRIMARY KEY, name TEXT, initial TEXT, address1 TEXT,"
+          "address2 TEXT, address3 TEXT, pincode TEXT, phone1 TEXT, phone2 TEXT, email TEXT)")
 
 # #inserting data to customer table
-# c.execute("INSERT INTO customer(name, initial, address1, address2, address3, pincode, phone1, phone2, email) VALUES ('TestName', 'Mr.', 'AddressTest1', 'AddressTest2', 'AddressTest3'"
-#           ", '302020', '9123193213', '12311432141', 'utkarsh@raghav.com' )")
-#
-# c.execute("INSERT INTO customer(name, initial, address1, address2, address3, pincode, phone1, phone2, email) VALUES ('TestName', 'Mr.', 'AddressTest1', 'AddressTest2', 'AddressTest3'"
-#           ", '302020', '9123193213', '12311432141', 'utkarsh@raghav.com' )")
-#
-# c.execute("INSERT INTO customer(name, initial, address1, address2, address3, pincode, phone1, phone2, email) VALUES ('TestName', 'Mr.', 'AddressTest1', 'AddressTest2', 'AddressTest3'"
-#           ", '302020', '9123193213', '12311432141', 'utkarsh@raghav.com' )")
+c.execute("INSERT INTO customer(name, initial, address1, address2, address3, pincode, phone1, phone2, email) VALUES ('TestName', 'Mr.', 'AddressTest1', 'AddressTest2', 'AddressTest3'"
+          ", '302020', '9123193213', '12311432141', 'utkarsh@raghav.com' )")
+
+c.execute("INSERT INTO customer(name, initial, address1, address2, address3, pincode, phone1, phone2, email) VALUES ('TestName', 'Mr.', 'AddressTest1', 'AddressTest2', 'AddressTest3'"
+          ", '302020', '9123193213', '12311432141', 'utkarsh@raghav.com' )")
+
+c.execute("INSERT INTO customer(name, initial, address1, address2, address3, pincode, phone1, phone2, email) VALUES ('TestName', 'Mr.', 'AddressTest1', 'AddressTest2', 'AddressTest3'"
+          ", '302020', '9123193213', '12311432141', 'utkarsh@raghav.com' )")
 
 # #creating items table
-# c.execute("CREATE TABLE IF NOT EXISTS itemDetails(itemNumber INTEGER PRIMARY KEY, itemName TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS itemDetails(itemNumber INTEGER PRIMARY KEY, itemName TEXT)")
 
 # #creating orders table
-# c.execute("CREATE TABLE IF NOT EXISTS orderDetails(orderId INTEGER PRIMARY KEY, custId INTEGER, itemNumber INTEGER, "
-#           "amountPaid INTEGER, amountDue INTEGER, totalAmount INTEGER,"
-#           "FOREIGN KEY(custId) REFERENCES customer(custId), FOREIGN KEY(itemNumber) REFERENCES itemDetails(itemNumber))")
+c.execute("CREATE TABLE IF NOT EXISTS orderDetails(orderId INTEGER PRIMARY KEY, custId INTEGER, itemNumber INTEGER, "
+          "amountPaid INTEGER, amountDue INTEGER, totalAmount INTEGER,"
+          "FOREIGN KEY(custId) REFERENCES customer(custId), FOREIGN KEY(itemNumber) REFERENCES itemDetails(itemNumber))")
 
 # #inserting data to itemDetails table
-# c.execute("INSERT INTO itemDetails(itemName) VALUES ('TestItemName4')")
-# c.execute("INSERT INTO itemDetails(itemName) VALUES ('TestItemName5')")
-# c.execute("INSERT INTO itemDetails(itemName) VALUES ('TestItemName6')")
+c.execute("INSERT INTO itemDetails(itemName) VALUES ('TestItemName1')")
+c.execute("INSERT INTO itemDetails(itemName) VALUES ('TestItemName2')")
+c.execute("INSERT INTO itemDetails(itemName) VALUES ('TestItemName3')")
 
 # #inserting data to orderDetails table
-# c.execute("INSERT INTO orderDetails(custId, itemNumber, amountPaid, amountDue, totalAmount) VALUES ('2', 2, 105, 40, 145)")
-# c.execute("INSERT INTO orderDetails(custId, itemNumber, amountPaid, amountDue, totalAmount) VALUES ('2', 3, 110, 24, 134)")
-# c.execute("INSERT INTO orderDetails(custId, itemNumber, amountPaid, amountDue, totalAmount) VALUES ('1', 3, 111, 10, 121)")
+c.execute("INSERT INTO orderDetails(custId, itemNumber, amountPaid, amountDue, totalAmount) VALUES (2, 2, 105, 40, 145)")
+c.execute("INSERT INTO orderDetails(custId, itemNumber, amountPaid, amountDue, totalAmount) VALUES (2, 3, 110, 24, 134)")
+c.execute("INSERT INTO orderDetails(custId, itemNumber, amountPaid, amountDue, totalAmount) VALUES (1, 3, 111, 10, 121)")
 
 db.commit()
 
